e958aaeed357662eb80864b2dea6f6331209da64
Max M <KarlWheezer>
2022-12-10 11:29:14 -0600

    update swerve creation code

--- src/main/java/frc/robot/commands/ExampleCommand.java
+++ src/main/java/frc/robot/commands/ExampleCommand.java
@@ -24,20 +24,16 @@ public class ExampleCommand extends CommandBase {
   }
 
   // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
+  @Override public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {}
+  @Override public void execute() {}
 
   // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
+  @Override public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
+  @Override public boolean isFinished() {
     return false;
   }
 }
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -17,7 +17,8 @@ public class Drivetrain extends SubsystemBase {
 
    //: Hardware
    private final Pigeon2 m_imu;
-   private final SwerveModule[] m_swerve_modules = new SwerveModule[4];
+   private SwerveModule m_frontLeft, m_frontRight,
+                        m_backLeft,  m_backRight;
 
    public Drivetrain() {
       //: IMU setup
@@ -30,14 +31,33 @@ public class Drivetrain extends SubsystemBase {
       var imuShuffleboard = tab.getLayout("IMU", BuiltInLayouts.kList)
          .withSize(2, 2).withPosition(8, 0);
       
-      imuShuffleboard.addNumber(
-         "Heading", () -> getHeading().getDegrees()
+      imuShuffleboard.addNumber( 
+         "Heading", () -> getHeading().getDegrees() 
       );
 
       Mk4ModuleConfiguration moduleConfiguration = new Mk4ModuleConfiguration();
       moduleConfiguration.setNominalVoltage(Constants.Drive.kDriveVoltageCompensation);
 
       //: Swerve setup
+      this.m_frontLeft = this.creatModule("Front Left Module", 10, moduleConfiguration, tab);
+      this.m_frontRight = this.creatModule("Front Right Module", 12, moduleConfiguration, tab);
+      this.m_backLeft = this.creatModule("Back Left Module", 14, moduleConfiguration, tab);
+      this.m_backRight = this.creatModule("Back Right Module", 16, moduleConfiguration, tab);
+   }
+
+   private SwerveModule creatModule(
+      String name, int main_number, 
+      Mk4ModuleConfiguration config, ShuffleboardTab tab
+   ) {
+      return Mk4SwerveModuleHelper.createNeo(
+         tab.getLayout(name, BuiltInLayouts.kList)
+            .withSize(2, 6).withPosition(0, main_number - 10),
+         
+         config, Mk4SwerveModuleHelper.GearRatio.L2,
+         main_number, //: drive IDe
+         main_number + 1, //: steering ID
+         11, 0 //: these are constant
+      );
    }
 
    @Override public void periodic() {}
@@ -49,5 +69,4 @@ public class Drivetrain extends SubsystemBase {
    public void resetHeading() {
       this.m_imu.setYaw(0);
    }
-
 }
