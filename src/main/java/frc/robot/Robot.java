9463b0127098f3c337225e80ca7bd439d4ed8b82
Ryan Sjostrand <ryansjostrand@gmail.com>
2024-01-01 18:02:31 -0600

    Pull in some Vision stuff from 2910

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.util.FieldConstants;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -43,6 +44,11 @@ public class Robot extends TimedRobot {
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
     m_robotContainer.robotPeriodic();
+
+    // auto start = System.currentTimeMillis();
+    FieldConstants.update();
+    // auto end = System.currentTimeMillis();
+    // Logger.getInstance().recordOutput("Times/FieldConstantsMs", end - start);
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
